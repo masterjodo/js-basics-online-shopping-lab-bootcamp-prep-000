f4fc2e2e5bac92626711fc6179d5c1494c3aa550
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 17:06:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,9 +47,8 @@ function placeOrder(cardNumber) {
     return("Sorry, we don't have a credit card on file for you.");
   }
   else {
-    var newtotal = total();
     cart = [];
-    return("Your total cost is $" + newtotal + ", which will be charged to the card " + cardNumber + ".");
+    return("Your total cost is $" + ${total} + ", which will be charged to the card " + cardNumber + ".");
   }
 
 }
