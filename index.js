2c89c276df5bca30a58d73dae46d52f577623ef5
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 16:07:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-
+  var itemName = {"itemName": item}
+  var itemPrice = {"itemPrice": Math.floor(Math.random() * 101)}
+  var itemObject = {itemName, itemPrice}
+  cart.push(itemObject)
 }
 
 function viewCart() {
