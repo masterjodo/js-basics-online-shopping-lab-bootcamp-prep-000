8f0f845eec7aeecf262dc07803903d800d31c159
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 16:34:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function viewCart() {
   // write your code here
 }
 
-var total = 0
 function total() {
+  total = 0;
   for (let i = 0; i < cart.length; i++) {
     total += cart[i].itemPrice;
   }
