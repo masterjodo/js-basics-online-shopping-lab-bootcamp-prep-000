8dc2af2bcd93f59f9cf86611ab7ac866a5c5fb29
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 16:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemName = {"itemName": item}
-  var itemPrice = {"itemPrice": Math.floor(Math.random() * 101)}
+  var itemPrice = {"itemPrice": (Math.floor(Math.random() * 101))}
   var itemObject = {itemName, itemPrice}
   cart.push(itemObject)
 }
