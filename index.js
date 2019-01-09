9eb70e9d7d1bbcfea434a967c53c62603488402f
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 17:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ function viewCart() {
   else if (cart.length === 1) {
     return ("In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ".")
   }
+  else if (cart.length === 2) {
+    return ("In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ".")
+  }
 }
 
 
